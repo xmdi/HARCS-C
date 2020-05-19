35e8eb76b27d1dff52b29eb491d869616f0c7fae
xmdi <xmdi@protonmail.com>
2020-05-19 03:18:03 -0400

    added R move

--- main.c
+++ main.c
@@ -3,28 +3,41 @@
 #include <stdlib.h>
 #include <time.h>
 
-void binaryOut(uint64_t number) {
-	if(number) {
-			binaryOut(number>>1);
-			putc((number&1)?'1':'0',stdout);
-	}
-}
-
-void printBinary(uint64_t number) {
-	printf("0b");
-	binaryOut(number);
-	printf("\n");
-}
-
 struct CUBE {
 	uint64_t EPCO,CPEOCN;
 };
 
+void binaryOut(uint64_t number) {
+	if (number) {
+		binaryOut(number>>1);
+		putc((number&1)?'1':'0',stdout);
+	}
+}
+
+void printCube(struct CUBE* cube) {
+	printf("\tEP:");
+	for (int i=0; i<12; ++i)
+		printf(" %x",0xf&(cube->EPCO>>(60-4*i)));
+	printf("\n\tEO:");
+	for (int i=0; i<12; ++i)
+		printf(" %x",0x3&(cube->CPEOCN>>(28-2*i)));
+	printf("\n\tCP:");
+	for (int i=0; i<8; ++i)
+		printf(" %x",0xf&(cube->CPEOCN>>(58-4*i)));
+	printf("\n\tCO:");
+	for (int i=0; i<8; ++i)
+		printf(" %x",0x3&(cube->EPCO>>(14-2*i)));
+	printf("\n\tCN:");
+	for (int i=0; i<2; ++i)
+		printf(" %x",0x7&(cube->CPEOCN>>(3-3*i)));
+	printf("\n");
+}
+
 void revertCube(struct CUBE* cube) {
-	//0011 0111 1011 1111 0010 0110 1010 1110 0001 0101 1001 1101 11 11 11 11 11 11 11 11 
-	cube->EPCO=0b0011011110111111001001101010111000010101100111011111111111111111; 
-	//1011 0011 0001 1001 1111 0111 0101 1101 11 11 11 11 11 11 11 11 11 11 11 11 001 010
-	cube->CPEOCN=0b10110011000110011111011101011101111111111111111111111111001010;
+	//0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 11 11 11 11 11 11 11 11 
+	cube->EPCO=0b0001001000110100010101100111100010011010101111001111111111111111;
+	//0001 0010 0011 0100 0101 0110 0111 1000 11 11 11 11 11 11 11 11 11 11 11 11 001 010
+	cube->CPEOCN=0b00010010001101000101011001111000111111111111111111111111001010;
 }
 
 void applyMove(struct CUBE* cube, char move) {
@@ -53,16 +66,84 @@ void applyMove(struct CUBE* cube, char move) {
 			cube->EPCO=(((0xfUL<<16)&cube->EPCO)<<12)|(((0xfffUL<<20)&cube->EPCO)>>4)|(((0x3UL)&cube->EPCO)<<6)|(((0xfcUL)&cube->EPCO)>>2)|(0xffffffff0000ff00&cube->EPCO);
 			cube->CPEOCN=(((0xfUL<<30)&cube->CPEOCN)<<12)|(((0xfffUL<<34)&cube->CPEOCN)>>4)|(((0x3UL<<6)&cube->CPEOCN)<<6)|(((0xfcUL<<6)&cube->CPEOCN)>>2)|(0x3fffc0003fffc03f&cube->CPEOCN);
 			break;
+		case 6: // R 
+			cube->EPCO=(((0xfUL<<36)&cube->EPCO)<<20)|
+				(((0xfUL<<56)&cube->EPCO)>>16)|
+				(((0xfUL<<40)&cube->EPCO)>>16)|
+				(((0XfUL<<24)&cube->EPCO)<<12)|
+				(3&((((((0x3UL<<10)&cube->EPCO)>>10)+1)>>2)+(((0x3UL<<10)&cube->EPCO)>>10)+1))<<12|
+				(3&((((((0x3UL<<12)&cube->EPCO)>>12)+2)>>2)+(((0x3UL<<12)&cube->EPCO)>>12)+2))<<4|
+				(3&((((((0x3UL<<4)&cube->EPCO)>>4)+1)>>2)+(((0x3UL<<4)&cube->EPCO)>>4)+1))<<2|
+				(3&((((((0x3UL<<2)&cube->EPCO)>>2)+2)>>2)+(((0x3UL<<2)&cube->EPCO)>>2)+2))<<10|
+				(0xf0fff00ff0ffc3c3&cube->EPCO);
+			cube->CPEOCN=(((0xfUL<<50)&cube->CPEOCN)<<4)|
+				(((0xfUL<<54)&cube->CPEOCN)>>16)|
+				(((0xfUL<<38)&cube->CPEOCN)>>4)|
+				(((0xfUL<<34)&cube->CPEOCN)<<16)|
+				(((0x3UL<<16)&cube->CPEOCN)<<10)|
+				(((0x3UL<<26)&cube->CPEOCN)>>8)|
+				(((0x3UL<<18)&cube->CPEOCN)>>8)|
+				(((0x3UL<<10)&cube->CPEOCN)<<6)|
+				(0x3c03fc03f3f0f3ff&cube->CPEOCN);
+			break;
+		case 7: // R2 
+			cube->EPCO=(((0xfUL<<36)&cube->EPCO)<<4)|
+				(((0xfUL<<56)&cube->EPCO)>>32)|
+				(((0xfUL<<40)&cube->EPCO)>>4)|
+				(((0XfUL<<24)&cube->EPCO)<<32)|
+				(((0x3UL<<10)&cube->EPCO)>>6)|
+				(((0x3UL<<12)&cube->EPCO)>>10)|
+				(((0x3UL<<4)&cube->EPCO)<<6)|
+				(((0x3UL<<2)&cube->EPCO)<<10)|
+				(0xf0fff00ff0ffc3c3&cube->EPCO);
+			cube->CPEOCN=(((0xfUL<<50)&cube->CPEOCN)>>12)|
+				(((0xfUL<<54)&cube->CPEOCN)>>20)|
+				(((0xfUL<<38)&cube->CPEOCN)<<12)|
+				(((0xfUL<<34)&cube->CPEOCN)<<20)|
+				(((0x3UL<<16)&cube->CPEOCN)<<2)|
+				(((0x3UL<<26)&cube->CPEOCN)>>16)|
+				(((0x3UL<<18)&cube->CPEOCN)>>2)|
+				(((0x3UL<<10)&cube->CPEOCN)<<16)|
+				(0x3c03fc03f3f0f3ff&cube->CPEOCN);
+			break;
+		case 8: // R' 
+			cube->EPCO=(((0xfUL<<36)&cube->EPCO)>>12)|
+				(((0xfUL<<56)&cube->EPCO)>>20)|
+				(((0xfUL<<40)&cube->EPCO)<<16)|
+				(((0XfUL<<24)&cube->EPCO)<<16)|
+				(3&((((((0x3UL<<10)&cube->EPCO)>>10)+1)>>2)+(((0x3UL<<10)&cube->EPCO)>>10)+1))<<2|
+				(3&((((((0x3UL<<12)&cube->EPCO)>>12)+2)>>2)+(((0x3UL<<12)&cube->EPCO)>>12)+2))<<10|
+				(3&((((((0x3UL<<4)&cube->EPCO)>>4)+1)>>2)+(((0x3UL<<4)&cube->EPCO)>>4)+1))<<12|
+				(3&((((((0x3UL<<2)&cube->EPCO)>>2)+2)>>2)+(((0x3UL<<2)&cube->EPCO)>>2)+2))<<4|
+				(0xf0fff00ff0ffc3c3&cube->EPCO);
+			cube->CPEOCN=(((0xfUL<<50)&cube->CPEOCN)>>16)|
+				(((0xfUL<<54)&cube->CPEOCN)>>4)|
+				(((0xfUL<<38)&cube->CPEOCN)<<16)|
+				(((0xfUL<<34)&cube->CPEOCN)<<4)|
+				(((0x3UL<<16)&cube->CPEOCN)>>6)|
+				(((0x3UL<<26)&cube->CPEOCN)>>10)|
+				(((0x3UL<<18)&cube->CPEOCN)<<8)|
+				(((0x3UL<<10)&cube->CPEOCN)<<8)|
+				(0x3c03fc03f3f0f3ff&cube->CPEOCN);
+			break;
+	
+	
+	
+	//0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 11 11 11 11 11 11 11 11 
+	
+	
+	
+	
+	
 	}
 }
 
-void benchmark(int quantity) {
+void benchmark(long quantity) {
 
 	struct CUBE cube;
 	revertCube(&cube);
 
 	clock_t start=clock(), diff;
-
 	for (int i=0; i<quantity/6; ++i) {
 		applyMove(&cube,0);
 		applyMove(&cube,1);
@@ -71,21 +152,43 @@ void benchmark(int quantity) {
 		applyMove(&cube,4);
 		applyMove(&cube,5);
 	}
-
 	diff=clock()-start;
 	int msec=diff*1000/CLOCKS_PER_SEC;
-
 	printf("\t%d [Ux/Dx] moves per second per thread\n",quantity/msec*1000);
 
-	printBinary(cube.EPCO);
-	printBinary(cube.CPEOCN);
+	start=clock();
+	for (int i=0; i<quantity/3; ++i) {
+		applyMove(&cube,6);
+		applyMove(&cube,7);
+		applyMove(&cube,8);
+	}
+	diff=clock()-start;
+	msec=diff*1000/CLOCKS_PER_SEC;
+	printf("\t%d [Rx] moves per second per thread\n",quantity/msec*1000);
+
+	printCube(&cube);
 
 }
 
+void testMove(char move) {
+
+	struct CUBE cube;
+	revertCube(&cube);
+	printf("\n BEFORE:\n");
+	printCube(&cube);
+
+	applyMove(&cube,move);
+
+	printf("\n AFTER:\n");
+	printCube(&cube);
+
+}
 
 int main() {
 	
 	benchmark(1e9);
+
+	//testMove(6);
 
 	return 0;
 }
